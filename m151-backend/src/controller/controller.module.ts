aab48e36a9faea58ea29d7ae00d9dd5bad65418a
Timon Schmid <timon.schmid@bluewin.ch>
2020-05-27 09:25:09 +0200

    added default cache

--- m151-backend/src/controller/controller.module.ts
+++ m151-backend/src/controller/controller.module.ts
@@ -1,15 +1,22 @@
-import {Module} from '@nestjs/common';
+import {CacheInterceptor, CacheModule, Module} from '@nestjs/common';
 import {AppController} from "./rest/app/app.controller";
 import {DomainModule} from "../domain/domain.module";
+import {APP_INTERCEPTOR} from "@nestjs/core";
 
 @Module({
     imports: [
         DomainModule,
+        CacheModule.register(), // TODO: Add Redis as caching store 'https://docs.nestjs.com/techniques/caching#different-stores'
     ],
     controllers: [
         AppController
     ],
-    providers: [],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CacheInterceptor,
+        },
+    ],
 })
 export class ControllerModule {
 }
