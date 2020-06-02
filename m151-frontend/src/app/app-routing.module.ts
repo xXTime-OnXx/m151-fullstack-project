a18ce37d2b5bd431319a1623ae5a4016fe9fe100
Timon Schmid <timon.schmid@bluewin.ch>
2020-06-02 23:37:45 +0200

    added login page

--- m151-frontend/src/app/app-routing.module.ts
+++ m151-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./page/login/login.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
